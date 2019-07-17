fa1c3ff1b0048649eb0a50a905f7db83dcd51058
Tucker Harris <tucker.harris121@gmail.com>
2019-07-17 10:09:08 -0400

    add barbajs v2, sync image classes

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -1,49 +1,17 @@
 // do not import Barba like this if you load the library through the browser
 import barba from '@barba/core'
 
-// basic default transition (with no rules and minimal hooks)
 barba.init({
   transitions: [
     {
-      leave({ current, next, trigger }) {
-        // do something with `current.container` for your leave transition
-        // then return a promise or use `this.async()`
+      sync: true,
+      leave(data) {
+        console.log('leaving' + data.current.namespace)
       },
-      enter({ current, next, trigger }) {
-        // do something with `next.container` for your enter transition
-        // then return a promise or use `this.async()`
+      enter(data) {
+        // transition that will play concurrently to `leave`
+        console.log('entering' + data.next.namespace)
       }
-    }
-  ]
-})
-
-// dummy example to illustrate rules and hooks
-barba.init({
-  transitions: [
-    {
-      name: 'dummy-transition',
-
-      // apply only when leaving `[data-barba-namespace="home"]`
-      from: 'home',
-
-      // apply only when transitioning to `[data-barba-namespace="products | contact"]`
-      to: {
-        namespace: ['detail']
-      },
-
-      // do leave and enter concurrently
-      sync: false,
-
-      // available hooks…
-      beforeAppear() {},
-      appear() {},
-      afterAppear() {},
-      beforeLeave() {},
-      leave() {},
-      afterLeave() {},
-      beforeEnter() {},
-      enter() {},
-      afterEnter() {}
     }
   ]
 })
