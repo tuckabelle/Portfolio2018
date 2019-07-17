c7f62b5f31cc5af5322250ced9bf0401cc58a899
Tucker Harris <tucker.harris121@gmail.com>
2019-07-17 08:59:44 -0400

    update changes

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -1,156 +1,49 @@
-// Page transitions
-// Call Barba.js when DOM is ready
-import Barba from 'barba.js'
-import { TweenMax, TimelineMax } from 'gsap/all'
+// do not import Barba like this if you load the library through the browser
+import barba from '@barba/core'
 
-document.addEventListener('DOMContentLoaded', function() {
-  let lastElementClicked
-  Barba.Pjax.init()
-  Barba.Prefetch.init()
-
-  Barba.Dispatcher.on('linkClicked', function(el) {
-    //lastElementClicked = el
-  })
-
-  const ExpandTransition = Barba.BaseTransition.extend({
-    start: function() {
-      Promise.all([this.newContainerLoading, this.loading()]).then(
-        this.movePages.bind(this)
-      )
-    },
-
-    loading: function() {
-      let deferred = Barba.Utils.deferred()
-      deferred.resolve()
-      return deferred.promise
-    },
-
-    movePages: function() {
-      let _this = this
-
-      _this.oldContainer.style.zIndex = '10'
-
-      let href = Barba.HistoryManager.currentStatus()
-        .url.split('/')
-        .pop()
-
-      let oldThumb = _this.oldContainer.querySelector(
-        'a[href*="/' + href[3] + '"] img'
-      )
-
-      let oldPos = oldThumb.getBoundingClientRect()
-
-      let destThumb = _this.newContainer.querySelector('.post-thumb')
-
-      let destPos = destThumb.getBoundingClientRect()
-
-      console.log(destPos)
-
-      _this.cloneThumb = oldThumb.cloneNode(true)
-      _this.cloneThumb.style.position = 'fixed'
-      _this.cloneThumb.style.top = oldPos.top + 'px'
-      _this.cloneThumb.style.left = oldPos.left + 'px'
-      _this.cloneThumb.style.right = oldPos.right + 'px'
-      _this.cloneThumb.height = oldPos.height
-      _this.cloneThumb.width = oldPos.width
-      _this.oldContainer.appendChild(_this.cloneThumb)
-
-      destThumb.style.visibility = 'hidden'
-      _this.newContainer.style.visibility = 'visible'
-      oldThumb.style.visibility = 'hidden'
-
-      setTransition(_this, -100, 100, destThumb)
-
-      TweenMax.to(_this.cloneThumb, 0.3, {
-        top: destPos.top,
-        left: window.screen.width - destPos.left,
-        right: destPos.right,
-        height: destThumb.clientHeight,
-        width: destThumb.clientWidth,
-        onComplete: function() {}
-      })
-    }
-  })
-
-  const ShrinkTransition = Barba.BaseTransition.extend({
-    start: function() {
-      Promise.all([this.newContainerLoading, this.loading()]).then(
-        this.movePages.bind(this)
-      )
-    },
-
-    loading: function() {
-      let deferred = Barba.Utils.deferred()
-      deferred.resolve()
-      return deferred.promise
-    },
-
-    movePages: function() {
-      let _this = this
-
-      _this.oldContainer.style.zIndex = '10'
-      //this.newContainer.style.visibility = 'visible'
-
-      let href = Barba.HistoryManager.prevStatus().url.split('/')
-
-      let destThumb = _this.newContainer.querySelector(
-        'a[href*="/' + href[3] + '"] img'
-      )
-      console.log(destThumb)
-      let destPos = destThumb.getBoundingClientRect()
-
-      let oldThumb = _this.oldContainer.querySelector('.post-thumb')
-
-      let oldPos = oldThumb.getBoundingClientRect()
-      _this.cloneThumb = oldThumb.cloneNode(true)
-      _this.cloneThumb.style.position = 'fixed'
-      _this.cloneThumb.style.top = oldPos.top + 'px'
-      _this.cloneThumb.height = oldPos.height
-      _this.cloneThumb.width = oldPos.width
-      _this.cloneThumb.style.zIndex = '30'
-      _this.oldContainer.appendChild(_this.cloneThumb)
-
-      destThumb.style.visibility = 'hidden'
-      _this.newContainer.style.visibility = 'visible'
-      oldThumb.style.visibility = 'hidden'
-
-      setTransition(_this, 100, -100, destThumb)
-
-      TweenMax.to(_this.cloneThumb, 0.3, {
-        position: 'fixed',
-        visibility: 'visible',
-        top: destPos.top,
-        left: -window.screen.width + destPos.left,
-        height: destThumb.clientHeight,
-        width: destThumb.clientWidth,
-        onComplete: function() {}
-      })
-    }
-  })
-
-  function setTransition(el, oldX, newX, destThumb) {
-    TweenMax.to(el.oldContainer, 0.5, {
-      xPercent: oldX
-    })
-
-    TweenMax.from(el.newContainer, 0.5, {
-      xPercent: newX,
-      onComplete: function() {
-        destThumb.style.visibility = 'visible'
-        TweenMax.set(el.newContainer, { clearProps: 'all' })
-        el.done()
+// basic default transition (with no rules and minimal hooks)
+barba.init({
+  transitions: [
+    {
+      leave({ current, next, trigger }) {
+        // do something with `current.container` for your leave transition
+        // then return a promise or use `this.async()`
+      },
+      enter({ current, next, trigger }) {
+        // do something with `next.container` for your enter transition
+        // then return a promise or use `this.async()`
       }
-    })
-  }
-
-  Barba.Pjax.getTransition = function() {
-    let transitionObj
-    if (Barba.HistoryManager.prevStatus().namespace === 'detail') {
-      transitionObj = ShrinkTransition
-    } else {
-      transitionObj = ExpandTransition
     }
+  ]
+})
 
-    return transitionObj
-  }
+// dummy example to illustrate rules and hooks
+barba.init({
+  transitions: [
+    {
+      name: 'dummy-transition',
+
+      // apply only when leaving `[data-barba-namespace="home"]`
+      from: 'home',
+
+      // apply only when transitioning to `[data-barba-namespace="products | contact"]`
+      to: {
+        namespace: ['detail']
+      },
+
+      // do leave and enter concurrently
+      sync: false,
+
+      // available hooks…
+      beforeAppear() {},
+      appear() {},
+      afterAppear() {},
+      beforeLeave() {},
+      leave() {},
+      afterLeave() {},
+      beforeEnter() {},
+      enter() {},
+      afterEnter() {}
+    }
+  ]
 })
