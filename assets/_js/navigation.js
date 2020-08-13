56082b8ed48d88b4d96bb1e616ba747e960d1bd0
Tucker Harris <tucker.harris121@gmail.com>
2020-08-12 20:48:24 -0400

    inital design change update

--- assets/_js/navigation.js
+++ assets/_js/navigation.js
@@ -28,3 +28,33 @@
 //
 //   lastScrollPos = scrollPos
 // })
+
+function resizeGridItem(item) {
+  let grid = document.getElementsByClassName('grid')[0]
+  let rowHeight = parseInt(
+    window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
+  )
+  let rowGap = parseInt(
+    window.getComputedStyle(grid).getPropertyValue('grid-row-gap')
+  )
+  let rowSpan = Math.ceil(
+    (item.querySelector('.content').getBoundingClientRect().height + rowGap) /
+      (rowHeight + rowGap)
+  )
+  item.style.gridRowEnd = 'span ' + rowSpan
+}
+
+function resizeAllGridItems() {
+  let allItems = document.getElementsByClassName('item')
+  for (let x = 0; x < allItems.length; x++) {
+    resizeGridItem(allItems[x])
+  }
+}
+
+function resizeInstance(instance) {
+  let item = instance.elements[0]
+  resizeGridItem(item)
+}
+
+window.onload = resizeAllGridItems()
+window.addEventListener('resize', resizeAllGridItems)
